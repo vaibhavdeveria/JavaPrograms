510fcb64aad93a025357a27ead3f1b3b9328b8b9
vaibhavdeveria <vaibhavdeveria09@gmail.com>
2021-08-16 16:56:38 -0400

    reverse no program

---
+++ src/com/testPackage/ReverseNo.java
@@ -0,0 +1,27 @@
+package com.testPackage;
+
+public class ReverseNo {
+
+	public  void reverse_no() {
+		
+		int no = 1234;
+		String rev = "";
+		
+		for(int i = 0; i<4; i++) {
+			//if(no!=0) {
+			int num = no%10; //3
+			rev = rev+num;  //43
+			no = no/10; //123
+			//}
+		}
+		System.out.println(rev);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ReverseNo obj = new ReverseNo();
+		obj.reverse_no();
+
+	}
+
+}
